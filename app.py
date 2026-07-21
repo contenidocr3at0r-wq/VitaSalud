50c6ba9c8116544dc45257b7292f87be63d192ff
contenidocr3at0r-wq <contenido.cr3at0r@gmail.com>
2026-07-21 11:27:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,6 @@ st.set_page_config(
         'Report a bug': None,
         'About': None
     }
-    st.markdown("""
-    <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
 )
 
 DEEPSEEK_API_KEY = "sk-16ba57fd0440454c848c268a55153fd1"
